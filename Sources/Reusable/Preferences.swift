6631623da13a031b6155933d5c0efec95522644b
Thomas SIVILAY <thomas.sivilay@gmail.com>
2020-02-24 06:23:47 +1100

    Open preferences to be overridden and add stored properties

--- Sources/Reusable/Preferences.swift
+++ Sources/Reusable/Preferences.swift
@@ -2,11 +2,11 @@ import Foundation
 
 open class Preferences {
     
-    fileprivate let store = NSUbiquitousKeyValueStore.default
+    public let store: NSUbiquitousKeyValueStore
     
-    public static let `default` = Preferences()
-    
-    private init() { }
+    public init(store: NSUbiquitousKeyValueStore) {
+        self.store = store
+    }
     
     public func configure() {
         NotificationCenter.default.addObserver(self, selector: #selector(storeDidChange), name: NSUbiquitousKeyValueStore.didChangeExternallyNotification, object: store)
@@ -29,21 +29,23 @@ public protocol PreferenceKey {
 
 @propertyWrapper
 public struct UbiquitousKeyValue<T> {
-    public let key: PreferenceKey
-    public let defaultValue: T
+    let key: PreferenceKey
+    let defaultValue: T
+    let store: NSUbiquitousKeyValueStore
     
-    public init(_ key: PreferenceKey, defaultValue: T) {
+    public init(_ key: PreferenceKey, defaultValue: T, store: NSUbiquitousKeyValueStore) {
         self.key = key
         self.defaultValue = defaultValue
+        self.store = store
     }
     
     public var wrappedValue: T {
         get {
-            Preferences.default.store.object(forKey: key.value) as? T ?? defaultValue
+            return store.object(forKey: key.value) as? T ?? defaultValue
         }
         set {
-            Preferences.default.store.set(newValue, forKey: key.value)
-            Preferences.default.synchronize()
+            store.set(newValue, forKey: key.value)
+            store.synchronize()
         }
     }
 }
